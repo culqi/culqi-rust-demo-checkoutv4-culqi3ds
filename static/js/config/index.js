686ac0cd05793ff509a1272dbd6557e1d998c1d9
JbyteSecurity <jordandiaz2016@gmail.com>
2023-09-04 10:13:10 -0500

    Initial Commit

---
+++ static/js/config/index.js
@@ -0,0 +1,24 @@
+//integracion
+export default Object.freeze({
+  TOTAL_AMOUNT: 600,
+  CURRENCY: "PEN",
+  PUBLIC_KEY: "pk_test_90667d0a57d45c48",
+  COUNTRY_CODE: "PE",
+  RSA_ID: "508fc232-0a9d-4fc0-a192-364a0b782b89",
+  RSA_PUBLIC_KEY: '-----BEGIN PUBLIC KEY-----'+
+  'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDYp0451xITpczkBrl5Goxkh7m1'+
+  'oynj8eDHypIn7HmbyoNJd8cS4OsT850hIDBwYmFuwmxF1YAJS8Cd2nes7fjCHh+7'+
+  'oNqgNKxM2P2NLaeo4Uz6n9Lu4KKSxTiIT7BHiSryC0+Dic91XLH7ZTzrfryxigsc'+
+  '+ZNndv0fQLOW2i6OhwIDAQAB'+
+  '-----END PUBLIC KEY-----',
+  URL_BASE: "http://localhost:3030",
+});
+
+export const customerInfo = {
+  firstName: "Fernando",
+  lastName: "Chullo",
+  address: "Coop. Villa el Sol",
+  address_c: "Palo Alto",
+  phone: "945737476",
+  email: "review1" + Math.floor(Math.random() * 100) + "@culqi.com",
+};
